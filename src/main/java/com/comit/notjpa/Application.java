44a478b1a130ffe2280b2883a98c28b1d7d68061
khacnam97 <31648392+khacnam97@users.noreply.github.com>
2024-10-03 11:34:11 +0700

    appp

--- src/main/java/com/comit/notjpa/Application.java
+++ src/main/java/com/comit/notjpa/Application.java
@@ -2,17 +2,22 @@ package com.comit.notjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
-//@EnableScheduling
-public class Application   {
+@EnableScheduling
+public class Application extends SpringBootServletInitializer {
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-//	@Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        System.out.println("Application Configure");
-//        return application.sources(Application.class);
-//    }
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		System.out.println("Application Configure");
+		return application.sources(Application.class);
+	}
 }
